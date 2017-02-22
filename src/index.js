19daf5200043fe7600748a72475a71c9d9013233
tanstaafl- <snedecor@live.unc.edu>
2017-02-22 16:43:55 -0500

    added GetTubeIntent function to handlers

--- src/index.js
+++ src/index.js
@@ -8,5 +8,13 @@ exports.handler = function(event, context, callback) {
 };
 
 var handlers = {
-
+  'GetTubeIntent': function () {
+    var test = this.event.request.intent.slots.Test;
+    if (test.value == "CBC" || test.value == "Complete Blood Count") {
+      this.emit(':tell', 'For the Complete Blood Count test, use the lavender tube.');
+    }
+    else if (test.value == "BMP" || test.value == "Basic Metabolic Panel") {
+      this.emit(':tell', 'For the Basic Metabolic Panel test, use the gold tube.');
+    }
+  }
 };
