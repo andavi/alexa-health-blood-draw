461311726c97c6bef784dce887b06802c0d937b4
tanstaafl- <snedecor@live.unc.edu>
2017-02-22 18:02:17 -0500

    GetTubeIntent works (if you spell out the tests)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 var Alexa = require("alexa-sdk");
 
-var APP_ID = "amzn1.ask.skill.b229e3a0-3ac1-4d0d-9c3b-cbf8131bde8d";
+//var APP_ID = "amzn1.ask.skill.b229e3a0-3ac1-4d0d-9c3b-cbf8131bde8d";
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -14,12 +14,19 @@ var handlers = {
         this.emit(':tell', 'Hello World!');
   },
   'GetTubeIntent': function () {
-    var test = this.event.request.intent.slots.Test;
-    if (test.value == "CBC" || test.value == "Complete Blood Count") {
+    var test = this.event.request.intent.slots.Test.value.toLowerCase();
+    console.log(test);
+    if (test == "complete blood count") {
+      console.log('found a CBC!');
       this.emit(':tell', 'For the Complete Blood Count test, use the lavender tube.');
     }
-    else if (test.value == "BMP" || test.value == "Basic Metabolic Panel") {
+    else if (test == "basic metabolic panel") {
+      console.log('found a BMP!');
       this.emit(':tell', 'For the Basic Metabolic Panel test, use the gold tube.');
+    }
+    else {
+      console.log('found nothing');
+      this.emit(':tell', 'Hello World!');
     }
   }
 };
