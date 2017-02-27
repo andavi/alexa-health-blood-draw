edd929bd792cab36bc2cf0a973568f90c05745b7
dsneds <snedecor@live.unc.edu>
2017-02-27 13:36:18 -0500

    testing framework in place, first two tests added

--- src/index.js
+++ src/index.js
@@ -3,30 +3,32 @@ var Alexa = require("alexa-sdk");
 
 //var APP_ID = "amzn1.ask.skill.b229e3a0-3ac1-4d0d-9c3b-cbf8131bde8d";
 
+/*
+  TESTING PROCEDURE:
+    - make sure you're in the src directory
+    - node_modules/.bin/mocha 
+*/
+
 exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  var alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
 
 var handlers = {
   'LaunchRequest': function () {
-        this.emit(':tell', 'Hello World!');
+        this.emit(':tell', 'Welcome to the Blood Draw skill!');
   },
   'GetTubeIntent': function () {
     var test = this.event.request.intent.slots.Test.value.toLowerCase();
-    console.log(test);
     if (test == "complete blood count") {
-      console.log('found a CBC!');
       this.emit(':tell', 'For the Complete Blood Count test, use the lavender tube.');
     }
     else if (test == "basic metabolic panel") {
-      console.log('found a BMP!');
       this.emit(':tell', 'For the Basic Metabolic Panel test, use the gold tube.');
     }
     else {
-      console.log('found nothing');
-      this.emit(':tell', 'Hello World!');
+      this.emit(':tell', 'Sorry, I don\'t have information for that test. Please ask about a different one.');
     }
   }
 };
