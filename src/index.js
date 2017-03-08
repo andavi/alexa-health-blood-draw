4f0dd5f99532982b427fe1f8a35598aec3f738e7
dsneds <snedecor@live.unc.edu>
2017-03-08 14:42:36 -0500

    multiple tests

--- src/index.js
+++ src/index.js
@@ -22,25 +22,31 @@ var handlers = {
         this.emit(':ask', 'Welcome to the Blood Draw skill! Which test would you like to know about?', 'Please specify a test.');
     },
     'GetTubeIntent': function() {
-        var test = this.event.request.intent.slots.Test.value.toLowerCase();
-        console.log('test -> ' + test);
-        if (!data.hasOwnProperty(test)) {
-            console.log('data doesn\'t have it');
-            test = test_map[test];
-            console.log('test is now -> ' + test);
+        var test_data = this.event.request.intent.slots.Test.value.toLowerCase();
+        console.log('test_data -> ' + test_data);
+        var tests = get_tests(test_data);
+        console.log(tests);
+        var s = "";
+        for (var i=0; i<tests.length; i++) {
+          /*if (!data.hasOwnProperty(test)) {
+              console.log('data doesn\'t have it');
+              test = test_map[test];
+              console.log('test is now -> ' + test);
+          }*/
+          //if (tests[i] != null) {
+              // var s = get_prefix(test) + ', use the ' + data[test]["tube"] + ' tube.';
+              var test = tests[i];
+              var quantity_suffix = data[test]["amount"] == 1 ? ' milliliter' : ' milliliters';
+              var info = data[test]["info"].length > 0 ? ' Remember, ' + data[test]["info"] + '.' : "";
+              s += ' ' + get_prefix(test) + ', use the ' + data[test]["tube"] + ' tube. It needs a quantity of ' + data[test]["amount"] + quantity_suffix + '.' + info;
+              console.log(s);
+          /*} else {
+              var s = 'Sorry, I don\'t have information for that test. Please ask about a different one.';
+              console.log(s);
+              this.emit(':tell', s);
+          }*/
         }
-        if (test != null) {
-            // var s = get_prefix(test) + ', use the ' + data[test]["tube"] + ' tube.';
-            var quantity_suffix = data[test]["amount"] == 1 ? ' milliliter' : ' milliliters';
-            var info = data[test]["info"].length > 0 ? ' Remember, ' + data[test]["info"] : "";
-            var s = get_prefix(test) + ', use the ' + data[test]["tube"] + ' tube. It needs a quantity of ' + data[test]["amount"] + quantity_suffix + '.' + info;
-            console.log(s);
-            this.emit(':tell', s);
-        } else {
-            var s = 'Sorry, I don\'t have information for that test. Please ask about a different one.';
-            console.log(s);
-            this.emit(':tell', s);
-        }
+        this.emit(':tell', s);
     }
 };
 
@@ -50,6 +56,45 @@ var get_prefix = function(test) {
     }
     return "For the " + test + " test";
 }
+
+var get_tests = function(test) {
+  var words = get_words(test);
+  var tests = [];
+  var current = "";
+  for (var i=0; i<words.length; i++) {
+    if (current.length > 0) {
+      current += " " + words[i];
+    }
+    else {
+      current = words[i];
+    }
+    if (data.hasOwnProperty(current)) {
+      tests.push(current);
+      current = "";
+    }
+    else if (test_map.hasOwnProperty(current)) {
+      tests.push(test_map[current]);
+      current = "";
+    }
+  }
+  return tests;
+}
+
+var get_words = function(test) {
+  var words = [];
+  var current = "";
+  for (var i=0; i<test.length; i++) {
+    if (test.charCodeAt(i) != 32) {
+      current += test[i]
+    }
+    else {
+      words.push(current);
+      current = "";
+    }
+  }
+  words.push(current);
+  return words;
+};
 
 var test_map = {
     "cbc": "complete blood count",
